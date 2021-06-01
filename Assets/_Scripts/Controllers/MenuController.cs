650e1e7bf27198e036a0ac42bfaea7d777abe30f
masonachu <57152092+masonachu@users.noreply.github.com>
2021-06-01 13:18:54 -0400

    Initial Build

---
+++ Assets/_Scripts/Controllers/MenuController.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class MenuController : MonoBehaviourPunCallbacks
+{
+    [SerializeField] private string gameVersion = "0.1";
+    [SerializeField] private GameObject UsernameMenu;
+    [SerializeField] private GameObject ConnectPanel;
+
+    [SerializeField] private InputField UsernameInput;
+    [SerializeField] private InputField CreateGameInput;
+    [SerializeField] private InputField JoinGameInput;
+
+    [SerializeField] private GameObject StartButton;
+
+
+
+    private void Awake()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    private void Start()
+    {
+        UsernameMenu.SetActive(true);
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        PhotonNetwork.JoinLobby();
+        Debug.Log("Connected");
+    }
+
+    public void ChangeUsernameInput()
+    {
+        if(UsernameInput.text.Length >= 3)
+        {
+            StartButton.SetActive(true);
+        }
+        else
+        {
+            StartButton.SetActive(false);
+        }
+    }
+
+    public void SetUserName()
+    {
+        UsernameMenu.SetActive(false);
+        PhotonNetwork.NickName = UsernameInput.text;
+    }
+
+    public void CreateGame()
+    {
+        RoomOptions roomOptions = new RoomOptions();
+        roomOptions.MaxPlayers = 16;
+        PhotonNetwork.CreateRoom(CreateGameInput.text, roomOptions, TypedLobby.Default);
+    }
+
+    public void JoinGame()
+    {
+        RoomOptions roomOptions = new RoomOptions();
+        roomOptions.MaxPlayers = 16;
+        PhotonNetwork.JoinOrCreateRoom("12345", roomOptions, TypedLobby.Default);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        PhotonNetwork.LoadLevel("MertBoxout2");
+    }
+}
---
+++ Assets/_Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject PlayerPrefab;
+    public GameObject GameCanvas;
+    public GameObject SceneCamera;
+
+    private void Awake()
+    {
+        GameCanvas.SetActive(true);
+    }
+
+    public void SpawnPlayer()
+    {
+        float randomValue = Random.Range(-1f, 1f);
+
+        PhotonNetwork.Instantiate(PlayerPrefab.name, new Vector3
+            (this.transform.position.x * randomValue, this.transform.position.y, 
+             this.transform.position.z), Quaternion.identity, 0);
+
+        GameCanvas.SetActive(false);
+        SceneCamera.SetActive(false);
+    }
+}
