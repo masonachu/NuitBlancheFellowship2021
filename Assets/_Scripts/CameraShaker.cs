4f4b3dfc4671f03a9bfe074daa4dc9bf54d40d97
masonachu <57152092+masonachu@users.noreply.github.com>
2021-08-03 15:21:24 -0400

    Updates on submerging sequence

--- Assets/_Scripts/CameraShaker.cs
+++ Assets/_Scripts/CameraShaker.cs
@@ -14,15 +14,24 @@ public class CameraShaker : MonoBehaviour
 
         myShakeInstance = Shaker.ShakeAll(EarthquakeShake);
         myShakeInstance.Stop(1f, false);
+        PlayerShaker.enabled = false;
     }
 
     public void BeginShaking() {
 
+        PlayerShaker.enabled = true;
         myShakeInstance.Start(0f);
     }
     
     public void EndShaking() {
 
         myShakeInstance.Stop(1f, false);
+        StartCoroutine(WaitForSeconds(1f));
+        PlayerShaker.enabled = false;
+    }
+
+    IEnumerator WaitForSeconds(float time) {
+
+        yield return new WaitForSeconds(time);
     }
 }
